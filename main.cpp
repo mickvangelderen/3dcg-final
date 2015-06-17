f34a93f1204d874f92bb69cc21b6ba4e7a1ba2ea
Mick van Gelderen <mickvangelderen@gmail.com>
2015-06-17 13:15:44 +0200

    Day and night

--- main.cpp
+++ main.cpp
@@ -25,21 +25,28 @@ using std::cout;
 using std::endl;
 using std::vector;
 using glm::vec3;
+using glm::vec4;
 using glm::ivec2;
 using glm::ivec3;
 using glm::mat4;
 
-vector<vec3> lights;
+vector<vec4> lights;
 DeltaTimer deltaTimer;
 
 void initializeLights() {
 	lights.resize(2);
-	lights[0] = vec3(1, 2, 3);
-	lights[1] = vec3(3, 4, 5);
+	lights[0] = vec4( 1.0f, 1.0f, 3.0f, 1.0f);
+	lights[1] = vec4(-2.0f, 4.0f, 5.0f, 1.0f);
+
+	float w[] = { 1, 1, 0.8f, 1 };
+	float b[] = { 0, 0.1f, 0.3f, 1 };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, w);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, b);
 }
 
-void drawLight(const mat4 & transform, const vec3 & light) {
-	mat4 local = glm::translate(transform, light);
+void drawLight(const mat4 & transform, const vec4 & light) {
+	if (light.w == 0.0f) return;
+	mat4 local = glm::translate(transform, vec3(light)/light.w);
 	glLoadMatrixf(glm::value_ptr(local));
 	glPushAttrib(GL_LIGHTING);
 	glDisable(GL_LIGHTING);
@@ -52,6 +59,7 @@ void drawLight(const mat4 & transform, const vec3 & light) {
 void drawAxes(float length = 1) {
 	glPushAttrib(GL_ALL_ATTRIB_BITS);
 	glDisable(GL_LIGHTING);
+	glColor3f(1, 1, 1);
 	glutSolidSphere(length/10.0f, 8, 8);
 	glBegin(GL_LINES);
 		glColor3f(1, 0, 0);
@@ -70,14 +78,17 @@ void drawAxes(float length = 1) {
 }
 
 ivec2 surfaceSize(40, 40);
-vec3 surfaceScale(8.0f, 8.0f, 1.0f);
+vec3 surfaceScale(8.0f, 8.0f, 2.0f);
 vec3 surfacePosition(0.0f);
 vector<float> surfaceHeights;
 vector<vec3> surfaceVertices;
 vector<ivec3> surfaceTriangles;
+vector<vec3> surfaceNormals;
+vector<vec3> surfaceColors;
 
 void initializeSurface() {
 	surfaceVertices.resize((surfaceSize.x + 1)*(surfaceSize.y + 1));
+	surfaceColors.resize((surfaceSize.x + 1)*(surfaceSize.y + 1));
 	surfaceTriangles = generate_grid(surfaceSize.x, surfaceSize.y);
 
 	vector<MountainGenerationLevel> levels(3);
@@ -85,8 +96,8 @@ void initializeSurface() {
 	levels[1].granularity = 0.5f; levels[1].min = -0.10f; levels[1].max = 0.10f;
 	levels[2].granularity = 1.0f; levels[2].min = -0.04f; levels[2].max = 0.04f;
 	surfaceHeights = generate_mountain(surfaceSize.x + 1, surfaceSize.y + 1, levels);
-	cout << surfaceHeights.size() << endl;
 
+	// Set vertex positions and colors.
 	for (int ix = 0; ix <= surfaceSize.x; ix++) {
 		for (int iy = 0; iy <= surfaceSize.y; iy++) {
 			int iv = iy + ix*(surfaceSize.y + 1);
@@ -94,26 +105,53 @@ void initializeSurface() {
 			float ry = (float) iy/surfaceSize.y - 0.5f;
 			float z = surfaceHeights[iv];
 			surfaceVertices[iv] = vec3(rx, ry, z);
+
+			if (z < 0.2) surfaceColors[iv] = vec3(113, 139, 77)/256.0f;
+			else if (z < 0.4) surfaceColors[iv] = vec3(79, 101, 58)/256.0f;
+			else if (z < 0.6) surfaceColors[iv] = vec3(166, 136, 100)/256.0f;
+			else if (z < 0.8) surfaceColors[iv] = vec3(116, 118, 141)/256.0f;
+			else surfaceColors[iv] = vec3(1, 1, 1);
 		}
 	}
 
+	// Clear normals.
+	surfaceNormals.assign((surfaceSize.x + 1)*(surfaceSize.y + 1), vec3(0.0f));
+
+	// Compute normals per triangle.
+	for (vector<ivec3>::size_type it = 0; it < surfaceTriangles.size(); it++) {
+		ivec3 vis = surfaceTriangles[it];
+		vec3 v1 = surfaceVertices[vis[0]];
+		vec3 v2 = surfaceVertices[vis[1]];
+		vec3 v3 = surfaceVertices[vis[2]];
+		vec3 n = glm::cross(v2 - v3, v2 - v1);
+		surfaceNormals[vis[0]] += n;
+		surfaceNormals[vis[1]] += n;
+		surfaceNormals[vis[2]] += n;
+	}
+
+	// Normalize normals.
+	for (vector<vec3>::size_type iv = 0; iv < surfaceNormals.size(); iv++) {
+		surfaceNormals[iv] = glm::normalize(surfaceNormals[iv]);
+	}
 }
 
 void drawSurface(const mat4 & transform) {
-	mat4 local = glm::translate(transform, surfacePosition);
-	local = glm::scale(local, surfaceScale);
-	glLoadMatrixf(glm::value_ptr(local));
-
 	glBegin(GL_TRIANGLES);
-	glColor3f(1, 0, 0);
-	for (vector<float>::size_type it = 0; it < surfaceTriangles.size(); it++) {
+	for (vector<ivec3>::size_type it = 0; it < surfaceTriangles.size(); it++) {
 		ivec3 vis = surfaceTriangles[it];
+		glColor3fv(glm::value_ptr(surfaceColors[vis[0]]));
+		glNormal3fv(glm::value_ptr(surfaceNormals[vis[0]]));
 		glVertex3fv(glm::value_ptr(surfaceVertices[vis[0]]));
+
+		glColor3fv(glm::value_ptr(surfaceColors[vis[1]]));
+		glNormal3fv(glm::value_ptr(surfaceNormals[vis[1]]));
 		glVertex3fv(glm::value_ptr(surfaceVertices[vis[1]]));
+
+		glColor3fv(glm::value_ptr(surfaceColors[vis[2]]));
+		glNormal3fv(glm::value_ptr(surfaceNormals[vis[2]]));
 		glVertex3fv(glm::value_ptr(surfaceVertices[vis[2]]));
 	}
 	glEnd();
-	glLoadMatrixf(glm::value_ptr(transform));
 }
 
 vec3 camera_position_velocity(0.0f, 0.0f, 0.0f);
@@ -133,50 +171,47 @@ void render()
 	glClear(GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT);
 
 	mat4 transform = glm::inverse(camera);
+
 	glLoadMatrixf(glm::value_ptr(transform));
 
-	drawAxes();
+	for (vector<vec3>::size_type il = 0; il < lights.size(); il++) {
+		glPushMatrix();
+			glTranslatef(lights[il].x, lights[il].y, lights[il].z);
+			glutSolidSphere(0.1, 8, 8);
+		glPopMatrix();
+		glLightfv(GL_LIGHT0 + il, GL_POSITION, glm::value_ptr(lights[il]));
+		// drawLight(transform, lights[il]);
+		// glLightfv(GL_LIGHT0 + il, GL_POSITION, glm::value_ptr(lights[il]));
+	}
 
+	// drawAxes();
+
+	mat4 surfaceTransform = glm::scale(transform, surfaceScale);
+	glLoadMatrixf(glm::value_ptr(surfaceTransform));
 	drawSurface(transform);
-
-	// glutSolidTeapot(3);
-
-
-
-	// glLoadMatrixf(transform);
-
-	// for (vector<vec3>::size_type il = 0; il < lights.size(); il++) {
-	// 	drawLight(transform, lights[il]);
-	// }
-	// glLightfv(GL_LIGHT0, GL_SPECULAR, whiteSpecularLight);
- //    glLightfv(GL_LIGHT0, GL_AMBIENT, blackAmbientLight);
- //    glLightfv(GL_LIGHT0, GL_DIFFUSE, whiteDiffuseLight);
- //    glLightfv(GL_LIGHT0, GL_POSITION, glm::value_ptr(LightPos));
-
-	// glPushMatrix();
-	// 	glTranslatef(0, 0, -4);
-	// 	glRotatef(cubeRotation, 1, .8, .6);
-	// 	glutSolidTeapot(1);
-	// glPopMatrix();
-
-	// drawCoordSystem();
+	glLoadMatrixf(glm::value_ptr(transform));
 
 	glutSwapBuffers();
 	glutPostRedisplay();
 }
 
+float l1rot = 0;
+
 void animate() {
 	float delta = deltaTimer.update();
 
-	vec3 lmov;
-	if (keys::held((unsigned char) 'd')) lmov.x = 1.0f;
-	if (keys::held((unsigned char) 'a')) lmov.x = -1.0f;
-	if (keys::held((unsigned char) 'w')) lmov.y = 1.0f;
-	if (keys::held((unsigned char) 's')) lmov.y = -1.0f;
-	if (keys::held((unsigned char) 'q')) lmov.z = 1.0f;
-	if (keys::held((unsigned char) 'z')) lmov.z = -1.0f;
-	lights[0] += 2*delta*lmov;
-	lights[1] += 3*delta*lmov;
+	l1rot += delta;
+	lights[0] = vec4(0.0f, 4*cos(l1rot), 4*sin(l1rot), 1.0f);
+
+	// vec3 lmov;
+	// if (keys::held((unsigned char) 'd')) lmov.x = 1.0f;
+	// if (keys::held((unsigned char) 'a')) lmov.x = -1.0f;
+	// if (keys::held((unsigned char) 'w')) lmov.y = 1.0f;
+	// if (keys::held((unsigned char) 's')) lmov.y = -1.0f;
+	// if (keys::held((unsigned char) 'q')) lmov.z = 1.0f;
+	// if (keys::held((unsigned char) 'z')) lmov.z = -1.0f;
+	// lights[0] += 2*delta*lmov;
+	// lights[1] += 3*delta*lmov;
 
 	vec3 crot(0.0f, 0.0f, 0.0f);
 	if (keys::held((unsigned char) 'l')) crot.z = -delta;
@@ -285,6 +320,7 @@ int main(int argc, char** argv) {
 	// Initialize opengl.
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
 	glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_DEPTH_TEST);
