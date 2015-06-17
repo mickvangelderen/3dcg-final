f18824d1a9345fff5e9d17028065c9e0c8b9908e
Mick van Gelderen <mickvangelderen@gmail.com>
2015-06-17 17:03:33 +0200

    Added sky

--- main.cpp
+++ main.cpp
@@ -100,10 +100,11 @@ void renderLights(const mat4 & transform) {
 
 // Surface.
 
-ivec2 surfaceSize(100, 100);
-vec3 surfaceScale(20.0f, 20.0f, 2.0f);
-vec2 surfaceView(0.0f, 0.0f);
 vec3 surfacePosition(0.0f, 0.0f, 0.0f);
+vec3 surfaceScale(5.0f, 10.0f, 4.0f);
+vec2 surfaceView(0.0f, 0.0f);
+ivec2 surfaceSize(50, 100);
+ivec2 surfaceHeightsSize(51, 801);
 vector<float> surfaceHeights;
 vector<vec3> surfaceVertices;
 vector<ivec3> surfaceTriangles;
@@ -115,11 +116,12 @@ void initializeSurface() {
 	surfaceColors.resize((surfaceSize.x + 1)*(surfaceSize.y + 1));
 	surfaceTriangles = generate_grid(surfaceSize.x, surfaceSize.y);
 
-	vector<MountainGenerationLevel> levels(3);
-	levels[0].granularity = 0.1f; levels[0].min =  0.00f; levels[0].max = 1.00f;
-	levels[1].granularity = 0.5f; levels[1].min = -0.10f; levels[1].max = 0.10f;
-	levels[2].granularity = 1.0f; levels[2].min = -0.04f; levels[2].max = 0.04f;
-	surfaceHeights = generate_mountain(surfaceSize.x + 1, surfaceSize.y + 1, levels);
+	vector<MountainGenerationLevel> levels(4);
+	levels[0].granularity = 0.05f; levels[0].min =  0.00f; levels[0].max = 1.00f;
+	levels[1].granularity = 0.20f; levels[1].min = -0.20f; levels[1].max = 0.20f;
+	levels[2].granularity = 0.50f; levels[2].min = -0.04f; levels[2].max = 0.04f;
+	levels[3].granularity = 1.00f; levels[3].min = -0.02f; levels[3].max = 0.02f;
+	surfaceHeights = generate_mountain(surfaceHeightsSize.x, surfaceHeightsSize.y, levels);
 }
 
 void drawSurface() {
@@ -140,17 +142,27 @@ void renderSurface(const mat4 & transform) {
 	// Set vertex positions and colors.
 	for (int ix = 0; ix <= surfaceSize.x; ix++) {
 		for (int iy = 0; iy <= surfaceSize.y; iy++) {
+			// Compute vertex index.
 			int iv = iy + ix*(surfaceSize.y + 1);
-			int viewx = (int) surfaceView.x;
-			int viewy = (int) surfaceView.y;
-			float rviewx = surfaceView.x - viewx;
-			float rviewy = surfaceView.y - viewy;
-			float rx = (ix - rviewx)/surfaceSize.x - 0.5f;
-			float ry = (iy - rviewy)/surfaceSize.y - 0.5f;
-			int ih = ((iy + viewy) % (surfaceSize.y + 1))
-				+ ((ix + viewx) % (surfaceSize.x + 1))*(surfaceSize.y + 1);
-			float z = surfaceHeights[ih];
-			surfaceVertices[iv] = vec3(rx, ry, z);
+
+			// Compute view coordinates.
+			int ixview = (int) surfaceView.x;
+			int iyview = (int) surfaceView.y;
+			float offsetx = (surfaceView.x - ixview)/surfaceSize.x;
+			float offsety = (surfaceView.y - iyview)/surfaceSize.y;
+
+			// Compute height map index.
+			int ixh = (ix + ixview) % surfaceHeightsSize.x;
+			int iyh = (iy + iyview) % surfaceHeightsSize.y;
+			int ih = iyh + ixh*surfaceHeightsSize.y;
+
+			// Compute x and y coordinates relative to the surface.
+			float rsx = (float) ix/surfaceSize.x;
+			float rsy = (float) iy/surfaceSize.y;
+
+			float c = (cos(rsx*glm::pi<float>()) + 1.0f)/2.0f; // Flatten 1 side.
+			float z = c*surfaceHeights[ih];
+			surfaceVertices[iv] = vec3(rsx - offsetx - 0.5, rsy - offsety - 0.5, z);
 
 			if (z < 0.2) surfaceColors[iv] = vec3(113, 139, 77)/256.0f;
 			else if (z < 0.4) surfaceColors[iv] = vec3(79, 101, 58)/256.0f;
@@ -203,7 +215,7 @@ void initializeCamera() {
 // Rendering
 
 void render() {
-	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glClearColor (69.0f/256.0f, 94.0f/256.0f, 209.0f/256.0f, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT);
 
 	mat4 transform = glm::inverse(camera);
